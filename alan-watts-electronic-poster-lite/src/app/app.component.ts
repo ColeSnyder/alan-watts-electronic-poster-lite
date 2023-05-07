42740ef79a793dd72cb4a217ae32ade5714783b7
Cole Snyder <csnyder14@winona.edu>
2023-05-07 08:55:51 -0500

    Added weather box, time box and author box along with author credit. Added AI generated images for watts and mckenna
    set timers and geo location in app component so we can get the current local weather and set weather box. Functions are called on timers reliably now

--- alan-watts-electronic-poster-lite/src/app/app.component.ts
+++ alan-watts-electronic-poster-lite/src/app/app.component.ts
@@ -4,7 +4,8 @@ import { fullWeatherResponse } from './home/weatherModels/fullWeatherResponse.mo
 import { weatherCodes } from './home/weatherModels/weatherCodes';
 
 interface Quote {
-  quote: string
+  quote: string,
+  author: string
 }
 
  @Component({
@@ -15,29 +16,66 @@ interface Quote {
 
 export class AppComponent implements OnInit {
   quotes: Quote[] = quotes;
-  displayedQuote: string = quotes[0].quote;
+  displayedQuote: string = "";
+  author: string = "";
   quoteCounter: number = 0;
   codes: weatherCodes = new weatherCodes();
   videoSource: string = "./assets/photos/vids/sun.mp4";
   currentWeatherCode: number = 0;
+  temperature: string = "0";
+  windSpeed: string = "0";
+  condition: string = "";
+  time: number = 0;
+  latitude: number = 0;
+  longitude: number = 0;
+  hour: string = "";
+  minutes: string = "";
 
   async ngOnInit() {
+    if (navigator.geolocation)
+    {
+      navigator.geolocation.getCurrentPosition((position) => {
+        this.latitude = position.coords.latitude;
+        this.longitude = position.coords.longitude;
+      });
+    } else {
+      console.log("Geolocation is not available for user");
+    }
+    this.checkCurrentWeather();
+    this.changeQuote();
+
+    await setInterval(() => this.display_ct(), 1000);
+    await setInterval(() => this.changeQuote(), 60000);
     await setInterval(this.checkCurrentWeather.bind(this), 1200000);
-    await setInterval(() => this.changeQuote(this.quoteCounter), 120000);
   }
 
-  async changeQuote(counter: number) {
-    if (counter == this.quotes.length) {
-      this.quoteCounter = 0;
-    }
-
-    this.displayedQuote = this.quotes[this.quoteCounter].quote;
-
+  changeQuote() {
+    var newQuoteIndex = Math.floor(Math.random() * (this.quotes.length - 0 + 1) + 0);
+    this.displayedQuote = this.quotes[newQuoteIndex].quote;
+    this.author = this.quotes[newQuoteIndex].author;
     this.quoteCounter++;
+
+    switch(this.quotes[newQuoteIndex].author) { 
+      case "Alan Watts": { 
+         var author = document.getElementById("authorBoxx");
+         author!.style.backgroundImage = "url('./assets/photos/alanWatts.jpg')";
+         break; 
+      } 
+      case "Terrance Mckenna":{ 
+         var author = document.getElementById("authorBoxx");
+         author!.style.backgroundImage = "url('./assets/photos/terranceMckenna.jpg')";
+         break; 
+      } 
+      default: { 
+         var author = document.getElementById("authorBoxx");
+         author!.style.backgroundImage = "./assets/photos/alanWatts.jpg";
+         break; 
+      } 
+   } 
   }
 
   async checkCurrentWeather() {
-    await fetch('https://api.open-meteo.com/v1/forecast?latitude=43.9844&longitude=-91.8693&timezone=CST&current_weather=true')
+    await fetch("https://api.open-meteo.com/v1/forecast?latitude=" + this.latitude + "&longitude=" + this.longitude + "&timezone=CST&current_weather=true&forecast_days=1")
       .then(res => res.json())
       .then(data => this.setBackgroundBasedOnWeatherCode(data));
   }
@@ -63,7 +101,80 @@ export class AppComponent implements OnInit {
      } else if (this.codes.rain.includes(weather.current_weather.weathercode)) {
         this.videoSource = "./assets/photos/vids/rain.mp4";
      } else if (this.codes.snow.includes(weather.current_weather.weathercode)) {
-      this.videoSource = "./assets/photos/vids/snow.mp4";
+        this.videoSource = "./assets/photos/vids/snow.mp4";
      }
+
+     this.setWeatherUnits(weather);
    }
+
+   setWeatherUnits(weather: fullWeatherResponse) {
+    this.temperature = this.roundedToFixed(weather.current_weather.temperature, 1);
+    this.windSpeed = this.roundedToFixed(weather.current_weather.windspeed / 1.609344, 1);
+    switch(weather.current_weather.weathercode) { 
+      case 0: { 
+         this.condition = "Clear Sky"
+         break; 
+      } 
+      case 1: 
+      case 2:
+      case 3:{ 
+         this.condition = "Partly Cloudy"
+         break; 
+      } 
+      case 45:
+      case 48:{ 
+         this.condition = "Fog"
+         break; 
+      } 
+      case 51:
+      case 53:
+      case 55:
+      case 56:
+      case 57:{ 
+         this.condition = "Drizzle"
+         break; 
+      } 
+      case 61:
+      case 63:
+      case 65:
+      case 80:
+      case 81:
+      case 82:
+      case 95:
+      case 96:
+      case 99:{ 
+         this.condition = "Rain"
+         break; 
+      } 
+      case 66:
+      case 67:{ 
+         this.condition = "Freezing Rain"
+         break; 
+      } 
+      case 71:
+      case 73:
+      case 75:
+      case 77:
+      case 85:
+      case 86:{ 
+         this.condition = "Rain"
+         break; 
+      }
+      default: { 
+         this.condition = "Unknown"
+         break; 
+      } 
+   } 
+   }
+
+   display_ct() {
+    var currentTime = new Date();
+    this.hour = currentTime.getHours().toString().length == 2 ? currentTime.getHours().toString() : "0" + currentTime.getHours().toString()
+    this.minutes = currentTime.getMinutes().toString().length == 2 ? currentTime.getMinutes().toString() : "0" + currentTime.getMinutes().toString();
+   }
+
+    roundedToFixed(input: number, digits: number){
+      var rounder = Math.pow(10, digits);
+      return (Math.round(input * rounder) / rounder).toFixed(digits).toString();
+    }
 }
